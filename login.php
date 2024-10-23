10a8d6212c7b688ec95d2e7e9b3272e2ba2a6650
Jervis <jervis344.89@gmail.com>
2024-10-23 11:12:11 -0600

    comment.

--- login.php
+++ login.php
@@ -3,4 +3,4 @@
     <input type="text" class="form-control" name="username" placeholder="Username" required>
     <input type="password" class="form-control" name="password" placeholder="Password" required>
     <button type="submit" class="btn btn-primary mt-2">Login</button>
-</form>
+</form> <--! comment -->
